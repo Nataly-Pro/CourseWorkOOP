80ae5f9b1e0e18aafda9cdc030966a60863f459b
natalya tomova <natalyayurevna19@gmail.com>
2023-09-19 21:47:38 +1000

    Добавлен классметод init_from_sj_data(), дополнены атрибуты класса Vacancy. Скорректирован метод init_from_hh_data().

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,55 +1,106 @@
+import re
+
+
 class Vacancy:
     """Класс для представления определенных полей вакансии"""
 
-    def __init__(self, name, url, salary, currency, requirement) -> None:
+    def __init__(self, name, url, area, employer, salary,
+                 currency, requirement) -> None:
         self.__check(name, url, salary)
 
-        self.name = name
-        self.url = url
-        self.salary = salary
+        self.__name = name
+        self.__url = url
+        self.area = area
+        self.employer = employer
+        self.__salary = salary
         self.currency = currency
         self.requirement = requirement
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def url(self):
+        return self.__url
+
+    @property
+    def salary(self):
+        return self.__salary
 
     @staticmethod
     def __check(name, url, salary):
         if not isinstance(name, str):
             raise TypeError('Должна быть строка.')
-        if not isinstance(salary, int) and salary != 'не указана':
+        if not isinstance(salary, int):
             raise TypeError('Должно быть целое число.')
         if not isinstance(url, str) and not url.startswith('https://'):
             raise TypeError('Ссылка на вакансию должна быть строкой '
                             'и начинаться на "https://".')
 
     @classmethod
-    def init_from_hh_data(cls, hh_data: list[dict]):
+    def init_from_hh_data(cls, hh_data: list[dict]) -> list['Vacancy']:
         """Класс - метод, инициализирующий экземпляры класса
         `Vacancy` данными о вакансиях, полученными с сайта hh.ru.
         :param hh_data: Список вакансий в виде словаря Python.
+
         """
         vacancies_list = []
         for vacancy in hh_data:
             name = vacancy['name']
-            url = vacancy['url']
-            requirement = vacancy['snippet']['requirement']
+            url = vacancy['alternate_url']
+            area = vacancy['area']['name']
+            employer = f"Работодатель: {vacancy['employer']['name']}"
+            if vacancy['snippet']['requirement'] is None:
+                requirement = 'Требования не указаны.'
+            else:
+                req = re.sub('</?highlighttext>', '',
+                             vacancy['snippet']['requirement'])
+                requirement = f"Требования: {req}"
 
-            if vacancy['salary'] is None:
-                salary = 'не указана'
+            if vacancy['salary'] is None or \
+                    vacancy['salary']['from'] is None:
+                salary = 0
                 currency = ''
             else:
-                currency = vacancy['salary']['currency']
-
-                if vacancy['salary']['from'] is None:
-                    salary = vacancy['salary']['to']
+                salary = vacancy['salary']['from']
+                if vacancy['salary']['currency'] == 'RUR':
+                    currency = 'RUB'
                 else:
-                    salary = vacancy['salary']['from']
+                    currency = vacancy['salary']['currency']
 
-            vacancy_obj = cls(name, url, salary, currency, requirement)
+            vacancy_obj = cls(name, url, area, employer,
+                              salary, currency, requirement)
+            vacancies_list.append(vacancy_obj)
+        return vacancies_list
+
+    @classmethod
+    def init_from_sj_data(cls, sj_data: list[dict]):
+        """Класс - метод, инициализирующий экземпляры класса
+        `Vacancy` данными о вакансиях, полученными с сайта superjob.ru.
+        :param sj_data: Список вакансий в виде словаря Python.
+
+        """
+        vacancies_list = []
+        for vacancy in sj_data:
+            name = vacancy['profession']
+            url = vacancy['link']
+            area = vacancy['town']['title']
+            employer = f"Работодатель: {vacancy['firm_name']}"
+            currency = vacancy['currency'].upper()
+            requirement = vacancy['candidat']
+            salary = vacancy['payment_from']
+
+            vacancy_obj = cls(name, url, area, employer,
+                              salary, currency, requirement)
             vacancies_list.append(vacancy_obj)
         return vacancies_list
 
     def __str__(self):
         return f'{self.name}\n' \
                f'{self.url}\n' \
+               f'{self.area}\n' \
+               f'{self.employer}\n' \
                f'{self.salary} {self.currency}\n' \
                f'{self.requirement}'
 
@@ -57,6 +108,6 @@ class Vacancy:
         """
         Метод сравнения вакансий между собой по зарплате.
         Возвращает результат сравнения (True/False).
+
         """
         return self.salary < other.salary
-
