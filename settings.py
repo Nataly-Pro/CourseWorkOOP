ac50e30876b0d3bb5ab863b590a7a9f991e4aee1
natalya tomova <natalyayurevna19@gmail.com>
2023-09-19 21:50:10 +1000

    Добавлен файл

---
+++ settings.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+ROOT_PATH = Path(__file__).parent
+PATH_TO_HH_FILE = Path.joinpath(ROOT_PATH, "hh_vacancies.json")
+PATH_TO_SJ_FILE = Path.joinpath(ROOT_PATH, "sj_vacancies.json")
